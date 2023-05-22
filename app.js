f915662a6a3d7274b5f379e8e1acae921824f578
Bejin raj <rajbejin@gmail.com>
2023-05-22 23:29:48 +0530

    cart count

--- app.js
+++ app.js
@@ -1,24 +1,29 @@
 let count = 0;
-let paragraph = document.getElementById("num","cart");
+let paragraph = document.getElementById("num");
+let cart = document.getElementById("cart");
 let incBtn = document.getElementById("plus-btn");
 let decBtn = document.getElementById("minus-btn");
 let addToCart = document.getElementById("addtocart");
 let add = document.getElementById("count");
 
 add.style.display = "none";
+cart.innerHtml= "Cart";
 
 incBtn.addEventListener("click", () => {
   count = count + 1;
   paragraph.innerHTML = count;
+  cart.innerHTML = `Cart ${count}`;
 });
 
 decBtn.addEventListener("click", () => {
   if (count > 0) {
     count = count - 1;
     paragraph.innerHTML = count;
+    cart.innerHTML = `Cart ${count}`;
     if (count === 0) {
       addToCart.style.display = "block";
       add.style.display = "none";
+      cart.innerHTML = "Cart";
     }
   }
 });
@@ -26,6 +31,7 @@ decBtn.addEventListener("click", () => {
 addToCart.addEventListener("click", () => {
   count = count + 1;
   paragraph.innerHTML = count;
+  cart.innerHTML = `Cart ${count}`;
   addToCart.style.display = "none";
   add.style.display = "flex";
 });
